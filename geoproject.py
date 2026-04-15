d23c245099c93b9fb8f1795abaadec34abb776bd
enzom <enzomanoel1980@gmail.com>
2026-04-15 12:06:55 -0300

    adição de pasta data para organização dos dados e alteração do nome da base de dados dados.xlsx para governador.xlsx

--- geoproject.py
+++ geoproject.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 from shapely.geometry import Point
 import matplotlib.pyplot as plt
 
-df = pd.read_excel("dados.xlsx")
+df = pd.read_excel("governador.xlsx")
 
 # Cria geometria a partir das colunas de coordenadas
 geometry = [Point(lon, lat) for lon, lat in zip(df["LONGITUDE"], df["LATITUDE"])]
